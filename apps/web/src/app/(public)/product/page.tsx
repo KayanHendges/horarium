8cb2514632540a236310940d3191232ef571a9b4
KayanHendges <kayanbhendges@gmail.com>
2024-07-17 10:32:01 -0300

    wip: ci-cd

--- apps/web/src/app/(public)/product/page.tsx
+++ apps/web/src/app/(public)/product/page.tsx
@@ -4,7 +4,6 @@ export default async function Product() {
   return (
     <div className="p-16 text-center">
       <Heading size="lg">
-        {process.env.NEXT_PUBLIC_API_BASE_URL || ""}
         Welcome to the <strong className="text-brand">Product</strong>
       </Heading>
     </div>
