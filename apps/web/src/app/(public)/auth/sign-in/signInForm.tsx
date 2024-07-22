1c52124695d9a9197882bebc46c88efd09f16fca
KayanHendges <kayanbhendges@gmail.com>
2024-07-22 11:53:16 -0300

    fix: suspense

--- apps/web/src/app/(public)/auth/sign-in/signInForm.tsx
+++ apps/web/src/app/(public)/auth/sign-in/signInForm.tsx
@@ -15,7 +15,7 @@ import { loginUserDTO } from "@repo/global";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { Suspense, useEffect } from "react";
+import { useEffect } from "react";
 
 export function SignInForm() {
   const {
@@ -62,54 +62,50 @@ export function SignInForm() {
   }, [pathname, router, searchParams]);
 
   return (
-    <Suspense>
-      <form
-        className="w-full flex flex-1 items-center justify-center py-4"
-        onSubmit={handleSignIn}
-      >
-        <Card className="w-full max-w-[400px]">
-          <CardHeader>
-            <Heading className="text-center">Entre com sua conta</Heading>
-          </CardHeader>
-          <CardContent className="flex flex-col gap-4">
-            <FormInput
-              label="Email"
-              {...register("email")}
-              errorMessage={errors.email?.message}
-            />
-            <FormInput label="Senha" errorMessage={errors.password?.message}>
-              <PasswordInput {...register("password")} />
-              <Link href={"/auth/forgot-password"}>
-                <Text className="underline hover:text-brand" size="md">
-                  Esqueci a senha
-                </Text>
-              </Link>
-            </FormInput>
-            <div className="flex flex-col gap-2">
-              <Button type="submit" isLoading={isSubmitting}>
-                Entrar
-              </Button>
-              <Button
-                type="button"
-                variant={"secondary"}
-                onClick={() => signInWithGoogle(router.push)}
-              >
-                <Image src={googleIcon} alt={""} className="w-5 h-5 mr-4" />
-                Entre com o Google
-              </Button>
-            </div>
-            <Separator />
-            <Text truncate={false}>
-              Ainda não possui conta?{" "}
-              <Link href={"/auth/sign-up"}>
-                <strong className="text-brand underline">
-                  Crie sua conta.
-                </strong>
-              </Link>
-            </Text>
-          </CardContent>
-        </Card>
-      </form>
-    </Suspense>
+    <form
+      className="w-full flex flex-1 items-center justify-center py-4"
+      onSubmit={handleSignIn}
+    >
+      <Card className="w-full max-w-[400px]">
+        <CardHeader>
+          <Heading className="text-center">Entre com sua conta</Heading>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-4">
+          <FormInput
+            label="Email"
+            {...register("email")}
+            errorMessage={errors.email?.message}
+          />
+          <FormInput label="Senha" errorMessage={errors.password?.message}>
+            <PasswordInput {...register("password")} />
+            <Link href={"/auth/forgot-password"}>
+              <Text className="underline hover:text-brand" size="md">
+                Esqueci a senha
+              </Text>
+            </Link>
+          </FormInput>
+          <div className="flex flex-col gap-2">
+            <Button type="submit" isLoading={isSubmitting}>
+              Entrar
+            </Button>
+            <Button
+              type="button"
+              variant={"secondary"}
+              onClick={() => signInWithGoogle(router.push)}
+            >
+              <Image src={googleIcon} alt={""} className="w-5 h-5 mr-4" />
+              Entre com o Google
+            </Button>
+          </div>
+          <Separator />
+          <Text truncate={false}>
+            Ainda não possui conta?{" "}
+            <Link href={"/auth/sign-up"}>
+              <strong className="text-brand underline">Crie sua conta.</strong>
+            </Link>
+          </Text>
+        </CardContent>
+      </Card>
+    </form>
   );
 }
